31bb3bf06dea39760dbbb8e4fdc74e468b803ae3
quanglong29074 <114908074+quanglong29074@users.noreply.github.com>
2023-08-17 15:08:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using bt3.Bank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(1000);
+
+            account.BalanceChanged += Account_BalanceChanged;
+
+            Console.WriteLine($"Current Balance: {account.Balance}");
+
+            account.Deposit(500);
+            account.Withdraw(2000);
+
+            Console.WriteLine($"Updated Balance: {account.Balance}");
+        }
+
+        private static void Account_BalanceChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Balance has been changed.");
+        }
+    }
+}
